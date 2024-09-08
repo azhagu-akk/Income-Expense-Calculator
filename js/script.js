2ec20f3eb1e2e42e54ac663456dd0d489d4e666f
Azhagu <azhagukarthikeyanb@gmail.com>
2024-09-08 07:48:37 +0530

    Added

---
+++ js/script.js
@@ -0,0 +1,103 @@
+// Dom Elements
+
+const balance = document.querySelector("#balance");
+const inc_amt = document.querySelector("#inc-amt");
+const exp_amt = document.querySelector("#exp-amt");
+const trans = document.querySelector("#trans");
+const form = document.querySelector("#form");
+const description = document.querySelector("#desc");
+const amount = document.querySelector("#amount");
+
+const localStorageTrans = JSON.parse(localStorage.getItem("trans"));
+let transactions =
+  localStorage.getItem("trans") !== null ? localStorageTrans : [];
+
+// Load Transaction Details
+
+  function loadTransactionDetails(transaction) {
+    const sign = transaction.amount < 0 ? "-" : "+";
+    const item = document.createElement("li");
+    item.classList.add("list");
+    item.classList.add(transaction.amount < 0 ? "exp" : "inc");
+    item.innerHTML = `
+    ${transaction.description}
+    <span>${sign} ${Math.abs(transaction.amount)}</span>
+    <button class="btn-del bg-red-500 text-white px-8 py-2 hover:cursor-pointer rounded-lg" onclick="removeTrans(${transaction.id})">Del</button>
+  `;
+    trans.appendChild(item);
+}
+  
+// Remove Transaction
+
+function removeTrans(id) {
+  if (confirm("Are you sure you want to delete Transcation?")) {
+    transactions = transactions.filter((transaction) => transaction.id != id);
+    config();
+    updateLocalStorage();
+  } else {
+    return;
+  }
+}
+
+// Update amount
+
+function updateAmount() {
+  const amounts = transactions.map((transaction) => transaction.amount);
+  const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+  balance.innerHTML = `₹  ${total}`;
+
+  const income = amounts
+    .filter((item) => item > 0)
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
+  inc_amt.innerHTML = `₹  ${income}`;
+
+  const expense = amounts
+    .filter((item) => item < 0)
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
+  exp_amt.innerHTML = `₹  ${Math.abs(expense)}`;
+}
+
+// Configuration
+
+function config() {
+  trans.innerHTML = "";
+  transactions.forEach(loadTransactionDetails);
+  updateAmount();
+}
+
+// Add Transaction
+
+function addTransaction(e) {
+  e.preventDefault();
+  if (description.value.trim() == "" || amount.value.trim() == "") {
+    alert("Please Enter Description and amount");
+  } else {
+    const transaction = {
+      id: uniqueId(),
+      description: description.value,
+      amount: +amount.value,
+    };
+    transactions.push(transaction);
+    loadTransactionDetails(transaction);
+    description.value = "";
+    amount.value = "";
+    updateAmount();
+    updateLocalStorage();
+  }
+}
+
+function uniqueId() {
+  return Math.floor(Math.random() * 10000000);
+}
+
+form.addEventListener("submit", addTransaction);
+
+window.addEventListener("load", function () {
+  config();
+});
+
+function updateLocalStorage() {
+  localStorage.setItem("trans", JSON.stringify(transactions));
+}
